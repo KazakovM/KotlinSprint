b91dbd189fbf4e7986fcf271be168a7a9c661d1b
MKazakov <57728172+KazakovM@users.noreply.github.com>
2023-10-01 10:47:28 +0300

    KS-1-1 solution for task

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,6 +1,6 @@
 package lesson_1
 
 fun main() {
-    var numberOfOrders: Long
-    var postOrderMessage: String
+    var numberOfOrders: Long = 75
+    var postOrderMessage: String = "Thank you for your order!"
 }
