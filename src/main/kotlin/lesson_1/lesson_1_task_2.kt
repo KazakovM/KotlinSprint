b9d77c029da25c3fc1b9e8f2722779c28aca783d
MKazakov <57728172+KazakovM@users.noreply.github.com>
2023-10-01 11:03:49 +0300

    KS-1-2 solution for task

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    var numberOfOrders: Long = 75
+    var postOrderMessage: String = "Thank you for your order!!!"
+    println(numberOfOrders)
+    println(postOrderMessage)
+    var numberOfEmployees: Int = 2000
+//    println(numberOfEmployees)
+    numberOfEmployees -= 1
+    println(numberOfEmployees)
+}
