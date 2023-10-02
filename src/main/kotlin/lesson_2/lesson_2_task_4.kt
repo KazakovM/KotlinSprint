a1fa48721073072240990eb316749549008c6c1d
KazakovM <kazakovms93@gmail.com>
2023-10-02 22:09:13 +0300

    KS-2-4 solution for task

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val numberOfCrystalOre: Int = 7
+    val numberOfIronOre: Int = 11
+    val buffedNumberOfCrystalOre = (numberOfCrystalOre * 0.2).toInt()
+    val buffedNumberOfIronOre = (numberOfIronOre * 0.2).toInt()
+    println(buffedNumberOfCrystalOre)
+    println(buffedNumberOfIronOre)
+}
