e62da6d9180c1f7d5d0125f6f29d03a0d42a4608
KazakovM <kazakovms93@gmail.com>
2023-10-02 21:43:50 +0300

    KS-2-1 solution for task

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+    val first: Byte = 3
+    val second: Byte = 4
+    val third: Byte = 3
+    val forth: Byte = 5
+    val average: Float = (first+second+third+forth) / 4f
+    println(average)
+}
