d706274cf47fbddbd5d0210c8f98ab5a6dee2c95
KazakovM <kazakovms93@gmail.com>
2023-10-02 21:27:55 +0300

    KS-1-5 fix after review

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -2,17 +2,8 @@ package lesson_1
 
 fun main() {
     val totalSeconds: Short = 6480
-    var hours: String = (totalSeconds / 3600).toString()
-    var minutes: String = (totalSeconds / 60 % 60).toString()
-    var seconds: String = (totalSeconds / 1 % 60).toString()
-    if (totalSeconds / 1 % 60 < 10) {
-        seconds = "0$seconds"
-    }
-    if (totalSeconds / 60 % 60 < 10) {
-        minutes = "0$minutes"
-    }
-    if (totalSeconds / 3600 < 10) {
-        hours = "0$hours"
-    }
-    println("$hours:$minutes:$seconds")
+    val hours: Int = totalSeconds / 3600
+    val minutes: Int = totalSeconds / 60 % 60
+    val seconds: Int = totalSeconds / 1 % 60
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
