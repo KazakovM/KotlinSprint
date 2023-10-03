bdc5d7e1c3d9eb374d208bdc7870f7b64ef2b3f2
KazakovM <kazakovms93@gmail.com>
2023-10-03 23:34:27 +0300

    KS-2-4 fix after review

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -3,8 +3,9 @@ package lesson_2
 fun main() {
     val numberOfCrystalOre: Int = 7
     val numberOfIronOre: Int = 11
-    val buffedNumberOfCrystalOre = (numberOfCrystalOre * 0.2).toInt()
-    val buffedNumberOfIronOre = (numberOfIronOre * 0.2).toInt()
+    val buffBonus: Float = 0.2F
+    val buffedNumberOfCrystalOre = (numberOfCrystalOre * buffBonus).toInt()
+    val buffedNumberOfIronOre = (numberOfIronOre * buffBonus).toInt()
     println(buffedNumberOfCrystalOre)
     println(buffedNumberOfIronOre)
 }
