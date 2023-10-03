085eb8893556d5a61e61454638dc92bde78d04cc
KazakovM <kazakovms93@gmail.com>
2023-10-03 23:54:30 +0300

    KS-3-1 solution for task

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main() {
+    val name = "Михаил"
+    var message = "Доброе утро"
+    println("$message, $name!")
+    message = "Добрый вечер"
+    println("$message, $name!")
+}
