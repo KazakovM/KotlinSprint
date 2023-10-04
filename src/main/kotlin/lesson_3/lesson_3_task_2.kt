7ea6bb9d41b779152e3c70015d8d7e7b8d203437
MKazakov <57728172+KazakovM@users.noreply.github.com>
2023-10-04 21:49:24 +0300

    KS-3-2 solution for task

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    var surname = "Андреева"
+    var name = "Татьяна"
+    var patronimic = "Сергеевна"
+    var age = 20
+    println("$surname $name $patronimic, $age")
+    surname = "Сидорова"
+    age = 22
+    println("$surname $name $patronimic, $age")
+}
