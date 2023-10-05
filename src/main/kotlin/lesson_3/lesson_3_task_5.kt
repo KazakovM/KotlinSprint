095fd964d463d3643666aa38e84c469c55cc0be3
KazakovM <kazakovms93@gmail.com>
2023-10-05 10:51:55 +0300

    KS-3-5 fix after review

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -2,8 +2,8 @@ package lesson_3
 
 fun main() {
     val moveRecord = "D2-D4;0"
-    val positionBefore = moveRecord.split(";")[0].split("-")[0]
-    val positionAfter = moveRecord.split(";")[0].split("-")[1]
+    val positionBefore = moveRecord.split("-", ";")[0]
+    val positionAfter = moveRecord.split("-", ";")[1]
     val moveCount = moveRecord.split(";")[1]
     println(positionBefore)
     println(positionAfter)
