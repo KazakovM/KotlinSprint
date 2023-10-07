34a4f07c84a1e252581c2a3ce48d69ddbe18f938
MKazakov <57728172+KazakovM@users.noreply.github.com>
2023-10-07 13:13:44 +0300

    KS-4-3 solution for task

---
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -0,0 +1,13 @@
+package lesson_4
+
+fun main() {
+    var isSunny = true
+    var isTentOpen = true
+    var humidity = 20
+    var season = "зима"
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+            "${isSunny && 
+               isTentOpen && 
+               humidity == 20 && 
+               season != "зима"}")
+}
