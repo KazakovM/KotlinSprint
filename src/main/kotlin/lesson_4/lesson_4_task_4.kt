50401d520816b8f840adde14b8dd7a46b254405e
KazakovM <kazakovms93@gmail.com>
2023-10-10 14:52:36 +0300

    KS-4-4 fix after review

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,11 +1,11 @@
 package lesson_4
 
 fun main() {
-    var dayNumber = 5
-    var armsDay = dayNumber % 2 == 1
-    var absDay = dayNumber % 2 == 1
-    var legsDay = dayNumber % 2 == 0
-    var backDay = dayNumber % 2 == 0
+    val dayNumber = 5
+    val armsDay = dayNumber % 2 == 1
+    val absDay = dayNumber % 2 == 1
+    val legsDay = dayNumber % 2 == 0
+    val backDay = dayNumber % 2 == 0
     println("Упражнения для рук: $armsDay\n" +
             "Упражнения для ног: $legsDay\n" +
             "Упражнения для спины: $backDay\n" +
