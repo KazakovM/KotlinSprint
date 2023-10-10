95a6b00de5a314c0dabb9a6e9211870081063368
KazakovM <kazakovms93@gmail.com>
2023-10-10 14:50:38 +0300

    KS-4-3 fix after review

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -4,7 +4,9 @@ fun main() {
     val isSunny = true
     val isTentOpen = true
     val humidity = 20
+    val goodHumidity = 20
     val season = "зима"
-    val isGoodCondition = isSunny && isTentOpen && humidity == 20 && season != "зима"
+    val badSeason = "зима"
+    val isGoodCondition = isSunny && isTentOpen && humidity == goodHumidity && season != badSeason
     println("Благоприятные ли условия сейчас для роста бобовых? $isGoodCondition")
 }
