6657b19e888063c13d691e9cf3a62cf07102e668
KazakovM <kazakovms93@gmail.com>
2023-10-10 14:44:09 +0300

    KS-4-2 fix after review

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -3,27 +3,26 @@ package lesson_4
 fun main() {
     val minWeight = 35
     val maxWeight = 100
+    val allowedWeight = minWeight..maxWeight
     val maxVolume = 100
-    var itemOneWeight = 20
-    var itemOneVolume = 80
-    var itemTwoWeight = 50
-    var itemTwoVolume = 100
+    val itemOneWeight = 20
+    val itemOneVolume = 80
+    val itemTwoWeight = 50
+    val itemTwoVolume = 100
     println(
         "Груз с весом $itemOneWeight кг и объемом $itemOneVolume л " +
                 "соответствует категории 'Average': " +
                 "${
-                    (itemOneWeight >= minWeight) &&
-                    (itemOneWeight <= maxWeight) &&
-                    (itemOneVolume < maxVolume)
+                    (itemOneWeight in allowedWeight &&
+                    itemOneVolume < maxVolume)
                 }"
     )
     println(
         "Груз с весом $itemTwoWeight кг и объемом $itemTwoVolume л " +
                 "соответствует категории 'Average': " +
                 "${
-                    (itemTwoWeight >= minWeight) &&
-                    (itemTwoWeight <= maxWeight) &&
-                    (itemTwoVolume < maxVolume)
+                    (itemTwoWeight in allowedWeight &&
+                    itemTwoVolume < maxVolume)
                 }"
     )
 }
